1cc9affae73c9575e1bd2dbb2c87ad70821be71a
Rheina <rheina.tamara@outlook.com>
2021-04-27 03:23:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var greeting = "Hello, everybody!";
 var specialGuest = "Neil deGrasse Tyson";
 var greetSpecialGuest = 'Hello, ' + specialGuest + '!';
 var conversation = 'space';
-var topic = `Let's talk about ${conversation}`;
+var topic = 'Let\'s talk about space';
